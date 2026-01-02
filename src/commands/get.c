6f904ce13d2aa90eec86b0e6cc24de02be502da9
henodell <relicxs1@gmail.com>
2026-01-02 17:10:49 +1100

    first commit

---
+++ src/commands/get.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "commands.h"
+
+void get() {
+    printf("Get func");
+}
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "commands.h"
+
+int main(int argc, char **argv) {
+    printf("%i\n", argc);
+    get();
+}
